c9cd7f858a4c0e1582a5cf5bb68ce34e7acbce0d
InvalidLenni <contact@invalidlenni.de>
2022-07-10 08:05:50 +0200

    Deploy

---
+++ src/data.js
@@ -0,0 +1,66 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "BrawlerStats",
+      subtitle: "Python",
+      description:
+        "BrawlerStats is a verified discord bot for viewing brawl stars profile and club stats in discord",
+      image: "./assets/brawlerstats_icon.png",
+      link: "https://bit.ly/BrawlerStats",
+    },
+    {
+      title: "Disweb",
+      subtitle: "Javascript with ExpressJS", 
+      description:
+        "DEPRECATED: Disweb is an API for get informations about a discord guild or user.",
+      image: "./assets/disweb.png",
+      link: "https://disweb.glitch.me/",
+    },
+    {
+      title: "LennCord",
+      subtitle: "PHP and PicoCSS",
+      description:
+        "LennCord will be a public chat app with many features.",
+      image: "./assets/avatar.png",
+      link: "#",
+    },
+    {
+      title: "GamerRoom",
+      subtitle: "Discord",
+      description:
+        "GamerRoom is a german discord server.",
+      image: "./assets/gamerroom.png",
+      link: "https://discord.gg/gamerroom",
+    },
+  ];
+
+export const skills = [
+  {
+    skill:"Python"
+  },
+  {
+    skill:"JavaScript"
+  },
+  {
+    skill:"Java"
+  },
+  {
+    skill:"Php"
+  },
+  {
+    skill:"Lua"
+  },
+  {
+    skill:"HTML"
+  },
+  {
+    skill:"CSS"
+  },
+  {
+    skill:"ReactJS"
+  },
+  {
+    skill:"TailwindCSS, PicoCSS, Material Design"
+  }
+]
